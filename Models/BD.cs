5cc70a26a9a7fc6a1e7e171efc002db636e366e2
MANUEL MANDEL <49518298@ort>
2025-07-08 12:06:42 -0300

    primer modulo de model BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,1 +1,22 @@
 using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace TP06.Models;
+
+public class BD
+{
+    private static string _connectionString = @"Server=localhost;DataBase=NombreBase;Integrated Security = True;TrustServerCertificate = True;";
+
+
+    int Login(string email, string contraseña)
+    {
+        int ID=-1;
+        using(SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT ID FROM Usuarios WHERE email = @pEmail AND contraseña = @pContraseña";
+            ID = connection.QueryFirstOrDefault<int>(query, new {pEmail = email, pContraseña = contraseña});
+        }
+        return ID;
+
+    }
+}
