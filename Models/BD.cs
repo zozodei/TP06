37f7bfe38a09bbc0184861da2f5d025641897532
MANUEL MANDEL <49518298@ort>
2025-07-10 13:38:09 -0300

    Terminado la clase BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -3,12 +3,12 @@ using Dapper;
 
 namespace TP06.Models;
 
-public class BD
+public static class BD
 {
     private static string _connectionString = @"Server=localhost;DataBase=NombreBase;Integrated Security = True;TrustServerCertificate = True;";
 
 
-    int Login(string email, string contraseña)
+    static int Login(string email, string contraseña)
     {
         int ID=-1;
         using(SqlConnection connection = new SqlConnection(_connectionString))
@@ -18,5 +18,37 @@ public class BD
         }
         return ID;
 
+    }
+
+    static Usuario GetUsuario(int ID)
+    {
+        Usuario usuario = null;
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT * FROM Usuarios WHERE ID = @pID";
+            usuario = connection.QueryFirstOrDefault<Usuario>(query, new {pID = ID});
+        }
+        return usuario;
+    }
+    static List<DatoFamiliar> GetDatoFamiliar(int id)
+    {
+        List<DatoFamiliar> familiares = null;
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT * FROM DatoFamiliar WHERE IdUsuario = @pID";
+            familiares = connection.Query<DatoFamiliar>(query, new {pID = id}).ToList();
+        }
+        return familiares;
+    }
+    
+    static List<DatoInteres> GetDatoInteres(int id)
+    {
+        List<DatoInteres> datosInteres = null;
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT * FROM DatoInteres WHERE IdUsuario = @pID";
+            datosInteres = connection.Query<DatoInteres>(query, new {pID = id}).ToList();
+        }
+        return datosInteres;
     }
 }
